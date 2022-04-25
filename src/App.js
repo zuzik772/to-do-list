f5a9baa807111f8634cd951f0291430c15c37dfd
Zuzana Chudinova <zuzka.chudinova@gmail.com>
2022-04-25 14:29:37 +0200

    add ListItem

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import ListItem from "./components/ListItem";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      task: "walk the dog",
+      completed: false,
+    },
+    {
+      id: 2,
+      task: "clean the house",
+      completed: true,
+    },
+  ]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <label htmlFor="name">Task</label>
+        <input type="text" id="name" name="task" />
+        <button>Add task</button>
+      </form>
+      <ul>
+        <ListItem />
+      </ul>
     </div>
   );
 }
---
+++ src/components/ListItem.jsx
@@ -0,0 +1,7 @@
+export default function ListItem(props) {
+  return (
+    <li>
+      Do the dishes <button>Delete</button>
+    </li>
+  );
+}

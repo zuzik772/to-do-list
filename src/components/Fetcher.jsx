2885f60663c92be035d2062ea07baa6551cf07f2
Zuzana Chudinova <zuzka.chudinova@gmail.com>
2022-04-26 12:57:30 +0200

    disable button when loading data

--- src/components/Fetcher.jsx
+++ src/components/Fetcher.jsx
@@ -3,21 +3,24 @@ import { useState, useEffect } from "react";
 export default function Fetcher(props) {
   const [character, setCharacter] = useState({ name: "Loading" }); //very hacky!
   const [count, setCount] = useState(1);
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     async function getData() {
       const res = await fetch("https://swapi.dev/api/people/" + count);
       const data = await res.json();
       setCharacter(data);
+      setLoading(false);
     }
     getData();
   }, [count]); //[count] means: re-run this function when count changes
   function nextCharacter() {
     setCount((old) => old + 1);
     setCharacter({ name: "Loading" });
+    setLoading(true);
   }
   return (
     <div>
-      <button disabled={true} onClick={nextCharacter}>
+      <button disabled={loading} onClick={nextCharacter}>
         Add 1
       </button>
       {count}-{character.name}
